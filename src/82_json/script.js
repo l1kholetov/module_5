21ef5a3d51e0c3d695ff3a97e33d537b79325de5
Vladimir <likholetov.vladimir2@yandex.ru>
2024-05-17 17:42:06 +0300

    add script.js in folder 82

---
+++ src/82_json/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const persone = {
+  name: 'Alex',
+  tel: '+74444444',
+  parents: {
+    mom: 'Olga',
+    dad: 'Mike'
+  }
+};
+
+const clone = JSON.parse(JSON.stringify(persone));
